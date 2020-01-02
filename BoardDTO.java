08b33c6b14c7c7ce176911423b38941a2aaba77a
Y-hopeworld <59408618+Y-hopeworld@users.noreply.github.com>
2020-01-02 10:38:02 +0900

    mybatis를 이용해 게시판 만들기
    게시글 등록,수정,삭제,조회,,검색,정렬(조회순으로),상세게시글

--- BoardDTO.java
+++ BoardDTO.java
@@ -1,72 +1,86 @@
-package problem.board;
+package board;
 
-import java.util.Date;
-
+import java.sql.Date;
 
 public class BoardDTO {
 
-	// private : 외부에서 사용할 수 없도록 숨겨두는 기능, 해당 클래스 내에서만 사용 가능
-	 private int bno;
-	 private String  title;
-	 private String content;
-	 private String writer;
-	 private int viewcnt;
-	 private Date regdate;
+	 private   int bno;   
+	 private   String title;
+	 private   String content;
+	 private   String writer;
+	 private   int viewcnt;
+	 private   Date regdate;
 	
-	 //priave 요소 접근 방법
-	 // 시스템변수나 사용자가 함부로 접근하면 안되는 요소들을 private 선언한다.
-	 // 하지만 private 요소들을 접근해서 사용 되어야 하는 경우에 제한적으로 접근하여 사용할 수 있게 만들어야한다
-	 // 1. 생성자로 접근  2. getter setter로 접근
+	 private BoardDTO() {}
+
 	 
-	 //기본생성자
-	 public BoardDTO() {}
 	 
-	 // 1. 생성자를 통해 접근
+	 
+	 
+	 public BoardDTO(String title, String content, String writer) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.writer = writer;
+	}
+
+
+
+
+
+	public BoardDTO(int bno) {
+		super();
+		this.bno = bno;
+	}
+
+
+
+
+
+	public BoardDTO(int bno, String title, String content, String writer) {
+		 super();
+		 this.bno = bno;
+		 this.title = title;
+		 this.content = content;
+		 this.writer = writer;
+		 this.regdate = regdate;
+	 }
+
+	 public BoardDTO(int bno, String title, String content, String writer, Date regdate) {
+			super();
+			this.bno = bno;
+			this.title = title;
+			this.content = content;
+			this.writer = writer;
+			this.regdate = regdate;
+		}
+	 
 	 public BoardDTO(int bno, String title, String content, String writer, int viewcnt, Date regdate) {
 		super();
-		// this.bno는 private변수인 bno를 가리키고 , bno는 생성자로 받아오는 매개변수(parameter)를 받아와서 접근  
 		this.bno = bno;
 		this.title = title;
 		this.content = content;
 		this.writer = writer;
 		this.viewcnt = viewcnt;
 		this.regdate = regdate;
-		
-	}
-
-	//게시물등록 
-	public BoardDTO(int bno, String title, String content, String writer) {
-		super();
-		this.bno = bno;
-		this.title = title;
-		this.content = content;
-		this.writer = writer;
-	}
-
-	
-	public BoardDTO(int bno, String title, String content, String writer, Date regdate) {
-		super();
-		this.bno = bno;
-		this.title = title;
-		this.content = content;
-		this.writer = writer;
-		this.regdate = regdate;
 	}
 	
+	 public BoardDTO(int viewcnt, int bno, String title, String writer, Date regdate, String content) {
+		 super();
+		 this.viewcnt = viewcnt;
+		 this.bno = bno;
+		 this.title = title;
+ 		 this.writer = writer;
+		 this.regdate = regdate;
+		 this.content = content;
+	 }
 	
-	//2. getter / setter 접근
-	 // 메서드를 통해 매개변수를 받아와서 private에 접근 시키는 방법
-	 // 1) 매개변수(paramenter):전달된 인자를 받아들이는 변수(variable)
-	 // 2) 인자 (argument) : 함수를 호출 할 대 전달되는 실제 값(value)
-	 
 
-	// getter :외부에서 private 값을 볼 수 있게 만드는 메서드 
+
 	public int getBno() {
 		return bno;
 	}
 
-	// setter : 외부에서 private 변수에 값을 넣어 영향을 미칠 수 있게 만든 메서드
-	// 직접적으로 private 변수에 접근하는게 아니라 개발자가 접근 방법을 지정
 	public void setBno(int bno) {
 		this.bno = bno;
 	}
@@ -103,6 +117,8 @@ public class BoardDTO {
 		this.viewcnt = viewcnt;
 	}
 
+	
+
 	public Date getRegdate() {
 		return regdate;
 	}
@@ -111,21 +127,15 @@ public class BoardDTO {
 		this.regdate = regdate;
 	}
 
+	@Override
 	public String toString() {
-		return + bno + "\t" 
-				+ title +"\t"
-				+ content + "\t"
-				+ writer + "\t"
-		    	+ viewcnt + "\t"
-				+regdate ;
+		return bno + "\t"  +  
+				title + "\t" + 
+				content + "\t" + 
+				writer+ "\t" +
+				viewcnt + "\t"+ 
+				regdate ;
 	}
-	 
-	 
-	 
-	 
-	 
-	 
-	 
 	 
 	 
 	 
--- BoardMain.java
+++ BoardMain.java
@@ -1,170 +1,158 @@
-package problem.board;
+package board;
 
 import java.util.Scanner;
-
-import problem.BOOK.BookDAO;
-
-// 게시판 만들기
-// 게시글 등록,수정,삭제,조회,검색,정렬,상세게시글,만든이,프로그램종료
-
 
 public class BoardMain {
 
 	public static void main(String[] args) {
-	
-		Scanner sc = new Scanner(System.in);
 
-		int code= 0;
-		
+			Scanner sc = new Scanner(System.in);
+			
+			int code;
+			
 		while(true) {
-		System.out.println("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨");
-		System.out.println("▨▨ 더블디 게시판 ");
-		System.out.println("▨▨ 1. 게시글 등록");
-		System.out.println("▨▨ 2. 게시글 수정");
-		System.out.println("▨▨ 3. 게시글 삭제");
-		System.out.println("▨▨ 4. 게시글 조회");
-		System.out.println("▨▨ 5. 게시글 검색");
-		System.out.println("▨▨ 6. 게시글 정렬");
-		System.out.println("▨▨ 7. 상세게시글");
-		System.out.println("▨▨ 8. 만든이");
-		System.out.println("▨▨ 9. 프로그램 종료");
-		System.out.println("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨");
-		
-		
-		
-		while(true) {
-			System.out.print("▨▨ 번호>>");
-			code = sc.nextInt();
+			System.out.println("■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■");
+			System.out.println("■□■□■□■□■□■□■□■□■□■□■□ Weverse 게시판 ■□■□■□■□■□■□■□■□■□■□■□■");
+			System.out.println("■□ 1. 게시글 등록");
+			System.out.println("■□ 2. 게시글 수정");
+			System.out.println("■□ 3. 게시글 삭제");
+			System.out.println("■□ 4. 게시글 조회");
+			System.out.println("■□ 5. 게시글 검색");
+			System.out.println("■□ 6. 게시글 정렬");
+			System.out.println("■□ 7. 상세게시글");
+			System.out.println("■□ 8. 만든이");
+			System.out.println("■□ 9. 프로그램 종료");
+			System.out.println("■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■");
+			
+			while(true) {
+				System.out.print("번호 >>");
+				 code = sc.nextInt();
+				
+				// 1~9까지 번호 입력 한경우 & 1~9 외의 번호를 입력한 경우
+					if(code >= 1 && code <= 9) {
+						break;
+					}else {
+						System.out.println("잘못된 번호입니다. 다시 입력해주세요.");
+						continue;
+					}
+			}  // break 나오는 곳
+				
+				BoardDAO bDao = new BoardDAO();
 			
 			
-			if(code >= 1 && code <= 9) {
-				break;
-			} else {
-				System.out.println("잘못된 번호 입력입니다. 1~9번 중에 다시 입력해주세요.");
-				continue;
-			}
-			
-		} 
+				// 1. 게시글 등록(제목,내용,작성자)
+				if(code == 1) {
+					System.out.print("게시글 제목 >>");
+					sc.nextLine();
+					String title =sc.nextLine();
+					
+					System.out.print("내용>>");
+					String content = sc.nextLine();
+					
+					System.out.print("작성자>>");
+					String writer = sc.nextLine();
+					
+					BoardDTO bDto = new BoardDTO(title, content, writer);
+					
+					bDao.insertBoard(title, content, writer);
+					
+					
+				 // 2. 수정	
+				}else if(code == 2) {
+					System.out.print("수정하고자 하는 게시물 번호>>");
+					int bno = sc.nextInt();
+					
+					System.out.print("제목>>");
+					sc.nextLine();
+					String title = sc.nextLine();
+					
+					System.out.print("내용>>");
+					String content = sc.nextLine();
+					
+					System.out.print("작성자>>");
+					String writer = sc.nextLine();
+					
+					
+					bDao.updateBoard(bno, title, content, writer);
+					
+				// 3. 삭제	
+				}else if(code == 3) {
+					System.out.println("삭제할 게시물의 번호를 입력하세요.");
+					System.out.print("번호>>");
+					int bno = sc.nextInt();
+					
+					bDao.deleteBoard(bno);
+					
+				// 4. 조회	
+				}else if(code == 4) {
+					System.out.println("전체 게시물을 조회합니다.");
+					
+					bDao.selectBoard();
+					
+				// 5. 검색	
+				}else if(code == 5) {
+					System.out.println("검색 할 내용을 입력해주세요.");
+					System.out.print("검색 >>");
+					sc.nextLine();
+					String keyword = sc.nextLine();
+					
+					bDao.searchBoard(keyword);
+					
+					
+					
+				// 6. 정렬	
+				}else if(code == 6) {
+					System.out.println("조회 순으로 정렬해주세요.");
+					bDao.boardSortBoard();
+					
+				// 7. 상세게시글	
+				}else if(code == 7) {
+					System.out.println("선택한 게시글을 조회합니다");
+					System.out.print("번호 >>");
+					int bno = sc.nextInt();
+				
+					bDao.viewBoard(bno);
+				// 8. 만든이
+				}else if(code == 8) {
+						System.out.println("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨");
+					 	System.out.println("▨▨ Name : Board Program ");
+					 	System.out.println("▨▨ Version : 1.7");
+					 	System.out.println("▨▨ Use : JAVA,ORACLE,MYBATIS");
+					 	System.out.println("▨▨ Date : 20.01.01");
+					 	System.out.println("▨▨ by 1Verse");
+					 	System.out.println("▨▨ Y.hopeworld@gmail.com");
+					
+				// 9. 프로그램종료	
+				}else if(code == 9) {
+						System.out.println("■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■");
+						System.out.println("■□■□■ 프로그램이 종료 되었습니다.");
+						System.exit(0);
+				
+				
+				}
 		
-			BoardDAO bDao = new BoardDAO();
-		  // 1. 게시물 등록
-		  if(code == 1) { 
-			  System.out.println("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨");
-			  System.out.print("▨▨ 1.게시물번호 >>");
-			  int bno = sc.nextInt();
-			  sc.nextLine();
-			  
-			  System.out.print("▨▨ 2.게시물제목 >>");
-			  String title = sc.nextLine();
-			  
-			  System.out.print("▨▨ 3.내용 >>");
-			  String content =sc.nextLine();
-			  
-			  System.out.print("▨▨ 4. 작성자>");
-			  String writer = sc.nextLine();
-			  
-			  // 게시물등록에 담을 내용을 BoardDTO에  
-			  BoardDTO bDto = new BoardDTO(bno, title, content, writer);
-			  bDao.boardInsert(bDto);
-			  
-			  
-		 // 2. 게시물 수정  : 바꾸고자 하는 게시물의 번호
-		  } else if(code == 2) {
 		
-			  System.out.println("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨");
-			  System.out.print("▨▨ 수정하고자 하는 게시물의번호 >>");
-			  int bno = sc.nextInt();
-			  sc.nextLine();
-			  
-			  System.out.print("▨▨ 게시물제목 >>");
-			  String title = sc.nextLine();
-			  
-			  System.out.print("▨▨ 게시물내용 >>");
-			  String content =sc.nextLine();
-			  
-			  System.out.print("▨▨ 작성자 >>");
-			  String writer =sc.nextLine();
-			  
-			
-			  BoardDTO bDto = new BoardDTO(bno, title, content, writer);
-			  
-			  bDao.boardUpdate(bDto);
-			  
-			  
-			  
-     	  // 3. 게시물 삭제
-		  }else if(code == 3) {
-			  System.out.println("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨");
-			  System.out.println("▨▨삭제하고자 하는 게시물의 번호를 입력하세요");
-			  System.out.print("▨▨ 번호 >>");
-			  int bno = sc.nextInt();
-			 
-			  bDao.boardDelete(bno);
-			  
-			  
-		  // 4. 게시물 조회
-		  }else if(code == 4) {
-			  System.out.println("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨");
-			  System.out.println("▨▨ 현재 등록된 게시물을 조회합니다.");
-			  System.out.println("▨▨번호 \t  제목 \t \t 내용 \t 작성자 \t 작성일자 ");
-			  bDao.boardSelect();
-			  
-			  
-		  // 5. 게시물 검색
-		  }else if(code == 5) {
-			  System.out.println("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨");
-			  System.out.println("▨▨ 검색 하려는 내용을 입력하세요>>");
-			  System.out.print("▨▨ 검색 >>");
-			  sc.nextLine();
-			  String search = sc.nextLine();
-			  
-			  bDao.boardSearch(search);
-			  
-			  
-			  
-		  // 6. 게사물 정렬
-		  }else if(code == 6) {
-			  System.out.println("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨");
-			  System.out.println("▨▨ 조회 순으로 게시물을 조회 해주세요>>");
-			  
-			  bDao.boardBoardSort();
-			  
-			  
-	      // 7. 상세게시글
-		  }else if(code == 7) {
-			  
-			  
-			  System.out.println("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨");
-			  System.out.println("▨▨ 보고싶은 게시글의 번호를 입력하세요 >>");
-			  System.out.print("▨▨ 번호 >>");
-			  int bno = sc.nextInt();
-			  
-			  bDao.boardView(bno);
-			  
-		  // 8. 만든이
-		  }else if(code == 8) {
-			  System.out.println("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨");
-			  System.out.println("▨▨ Name : Board Program ");
-			  System.out.println("▨▨ Version : 1.7");
-			  System.out.println("▨▨ Use : JAVA,ORACLE");
-			  System.out.println("▨▨ Date : 19.12.28");
-			  System.out.println("▨▨ by 1Verse");
-			  System.out.println("▨▨ Y.hopeworld@gmail.com");
-			  
-			  
-		  // 9.프로그램 종료  
-		  }else if(code == 9) {
-			  System.out.println("▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨▨");
-			  System.out.println("▨▨프로그램이 종료되었습니다.");
-			  System.exit(0);
-			  
-		  }
-		  
 		
+		
+		
+		
+		
+		
+	    
+		
+		
+		
+		
+		
+		}
+
 	
-		
-		
 	}
 
-	}
+
+
+
+
+
+
 }
